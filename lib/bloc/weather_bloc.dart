cd85b3a349a20321a119f7eecaa90c385e30c3bf
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-04 22:13:17 +0200

    replace initialState with constructor

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -2,6 +2,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_event.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_state.dart';
 
-class WeatherBloc extends Bloc<WeatherEvent, WeatherState>{
-  WeatherBloc(super.initialState);
+class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
+  WeatherBloc()
+      : super(const WeatherState(
+            isLoading: false, permissionState: PermissionState.declined));
 }
