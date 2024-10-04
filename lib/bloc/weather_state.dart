6b3f1e59104c57e6273c4daefbe1c96e05b41724
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-04 22:38:32 +0200

    WIP implement weather_state.dart code

--- lib/bloc/weather_state.dart
+++ lib/bloc/weather_state.dart
@@ -1,16 +1,30 @@
 import 'package:equatable/equatable.dart';
 
-class WeatherState extends Equatable{
+import '../model/weather_forecast.dart';
 
+class WeatherState extends Equatable {
   final bool isLoading;
   final PermissionState permissionState;
+  final List<WeatherForecast> weatherForecast;
 
-  const WeatherState({required this.isLoading, required this.permissionState});
+  const WeatherState(
+      {this.weatherForecast = const [],
+      this.isLoading = false,
+      this.permissionState = PermissionState.declined});
 
   @override
   List<Object?> get props => throw UnimplementedError();
+
+  WeatherState copyWith({
+    List<WeatherForecast>? weatherForecast,
+    bool? isLoading,
+    PermissionState? permissionState,
+  }) {
+    return WeatherState(
+        weatherForecast: weatherForecast ?? this.weatherForecast,
+        isLoading: isLoading ?? this.isLoading,
+        permissionState: permissionState ?? this.permissionState);
+  }
 }
 
 enum PermissionState { granted, declined }
-
-
