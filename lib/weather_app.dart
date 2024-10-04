32e9380f37020258521dc20ad8a5a7b182f1ea8c
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-04 22:12:42 +0200

    init router setup in weather_app.dart

--- lib/weather_app.dart
+++ lib/weather_app.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_weather_forecast_app/bloc/weather_bloc.dart';
+import 'package:flutter_weather_forecast_app/presentation/weather_app_main_screen.dart';
+import 'package:go_router/go_router.dart';
 
 import 'module/module.dart';
 
@@ -8,10 +12,27 @@ class WeatherApp extends StatelessWidget {
 
   final Module module;
 
+  GoRouter getRouter(Module module) {
+    return GoRouter(routes: [
+      GoRoute(
+          path: '/',
+          builder: (context, state) => BlocProvider(
+                create: (context) => WeatherBloc(),
+                child: const WeatherAppMainScreen(),
+              ))
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+
+    var router = getRouter(module);
+
+    return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      routerDelegate: router.routerDelegate,
+      routeInformationParser: router.routeInformationParser,
+      routeInformationProvider: router.routeInformationProvider,
     );
   }
 }
