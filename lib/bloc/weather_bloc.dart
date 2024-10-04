6cb4d231924a105384aa808f097bdfc3037bc12f
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-04 22:39:56 +0200

    WIP implement bloc _checkLocationPermissionEvent

--- lib/bloc/weather_bloc.dart
+++ lib/bloc/weather_bloc.dart
@@ -1,9 +1,27 @@
+import 'dart:async';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_event.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_state.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 class WeatherBloc extends Bloc<WeatherEvent, WeatherState> {
-  WeatherBloc()
-      : super(const WeatherState(
-            isLoading: false, permissionState: PermissionState.declined));
+  WeatherBloc() : super(const WeatherState()){
+
+  on<CheckLocationPermissionEvent>(_checkLocationPermissionEvent);
+  }
+
+
+FutureOr<void> _checkLocationPermissionEvent(
+    CheckLocationPermissionEvent event,
+    Emitter<WeatherState> emit,
+    ) async {
+        var  status = await Permission.location.status;
+
+        if(status.isGranted){
+
+        }else {
+            emit(state.copyWith(permissionState: PermissionState.declined));
+        }
+    }
 }
