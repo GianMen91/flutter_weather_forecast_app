52ea58e3453ecc4c46e4b8aed2b95178443b9507
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-06 00:16:56 +0200

    move Permission widget code to a separate file

--- lib/presentation/weather_app_main_screen.dart
+++ lib/presentation/weather_app_main_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_bloc.dart';
 import 'package:flutter_weather_forecast_app/bloc/weather_state.dart';
 
-import '../bloc/weather_event.dart';
+import '../widgets/permission_widget.dart';
 
 class WeatherAppMainScreen extends StatelessWidget {
   const WeatherAppMainScreen({super.key});
@@ -23,41 +22,5 @@ class WeatherAppMainScreen extends StatelessWidget {
   }
 }
 
-class PermissionWidget extends StatelessWidget {
-  const PermissionWidget({super.key, required this.bloc});
 
-  final WeatherBloc bloc;
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                shape: BoxShape.circle
-            ),
-            child: const Icon(Icons.location_on, size: 100,),
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30,vertical: 15),
-            child: Text('Allow location permission to get your city location', textAlign: TextAlign.center),
-          ),
-          GestureDetector(
-            onTap: ()=> bloc.add(AskForLocationPermissionEvent()),
-            child: Container(
-              padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
-              decoration:  BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: BorderRadius.circular(20)
-              ),
-              child: const Text('Give Permission', style: TextStyle(color: Colors.white),),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
 
