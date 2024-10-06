4ce110279f5fde9736c9a252407280fd38607bda
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-06 03:49:24 +0200

    simplify code and manage errors

--- lib/network/open_weather_api_call.dart
+++ lib/network/open_weather_api_call.dart
@@ -1,77 +1,88 @@
 import 'dart:collection';
+import 'dart:convert';
 
 import 'package:flutter_weather_forecast_app/model/weather_forecast.dart';
-import 'package:flutter_weather_forecast_app/network/app_url.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class OpenWeatherApiCall {
-  final AppUrl _appUrl;
-  final Dio _dio;
 
-  OpenWeatherApiCall({required AppUrl appUrl, required Dio dio})
-      : _appUrl = appUrl,
-        _dio = dio;
 
   Future<List<WeatherForecast>> loadWeatherForecast(
       double lon,
       double lat,
       ) async {
-    final response = await _dio.get(_appUrl.weatherForecast(lon, lat));
-    if (response.statusCode == 200) {
-      if (response.data['list'] is List &&
-          (response.data['list'] as List).isNotEmpty) {
-        Map<String, List<WeatherForecast>> groupedForecasts = {};
-        for (var item in response.data['list']) {
-          String date =
-          item['dt_txt'].substring(0, 10); // Extracting the date part
-          WeatherForecast forecast = WeatherForecast(
-              date: date,
-              temperature: (item['main']['temp'] as num).toDouble(),
-              humidity: (item['main']['humidity'] as num).toDouble(),
-              wind: (item['wind']['speed'] as num).toDouble(),
-              pressure: (item['main']['pressure'] as num).toDouble(),
-              cloudiness: (item['clouds']['all'] as num).toDouble(),
-              weatherCondition:
-              _parseWeatherCondition(item['weather'][0]['main']));
+    String baseUrl = 'https://api.openweathermap.org/data/2.5/forecast';
+    String apiKey = '2490e19bf3443658945b392efebeae7c';
 
-          if (!groupedForecasts.containsKey(date)) {
-            groupedForecasts[date] = [];
+    String url =
+        '$baseUrl/?appid=$apiKey&lat=$lat&lon=$lon&exclude=hourly,daily&units=metric';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        // Parse the response body into a JSON object
+        final jsonData = jsonDecode(response.body);
+
+        // Check if 'list' is a valid list and contains elements
+        if (jsonData['list'] is List && (jsonData['list'] as List).isNotEmpty) {
+          Map<String, List<WeatherForecast>> groupedForecasts = {};
+
+          for (var item in jsonData['list']) {
+            String date = item['dt_txt'].substring(0, 10);
+            WeatherForecast forecast = WeatherForecast(
+                date: date,
+                temperature: (item['main']['temp'] as num).toDouble(),
+                humidity: (item['main']['humidity'] as num).toDouble(),
+                wind: (item['wind']['speed'] as num).toDouble(),
+                pressure: (item['main']['pressure'] as num).toDouble(),
+                cloudiness: (item['clouds']['all'] as num).toDouble(),
+                weatherCondition:
+                _parseWeatherCondition(item['weather'][0]['main']));
+
+            if (!groupedForecasts.containsKey(date)) {
+              groupedForecasts[date] = [];
+            }
+            groupedForecasts[date]!.add(forecast);
           }
-          groupedForecasts[date]!.add(forecast);
+
+          List<WeatherForecast> averagedForecasts = [];
+          groupedForecasts.forEach((date, forecasts) {
+            double avgTemp =
+                forecasts.map((f) => f.temperature).reduce((a, b) => a + b) /
+                    forecasts.length;
+            double avgHumidity =
+                forecasts.map((f) => f.humidity).reduce((a, b) => a + b) /
+                    forecasts.length;
+            double avgWind =
+                forecasts.map((f) => f.wind).reduce((a, b) => a + b) /
+                    forecasts.length;
+            double avgPressure =
+                forecasts.map((f) => f.pressure).reduce((a, b) => a + b) /
+                    forecasts.length;
+            double avgCloudiness =
+                forecasts.map((f) => f.cloudiness).reduce((a, b) => a + b) /
+                    forecasts.length;
+            WeatherCondition avgWeatherCondition =
+            _determineMostFrequentCondition(forecasts);
+
+            averagedForecasts.add(WeatherForecast(
+              date: date,
+              temperature: avgTemp,
+              humidity: avgHumidity,
+              wind: avgWind,
+              pressure: avgPressure,
+              cloudiness: avgCloudiness,
+              weatherCondition: avgWeatherCondition,
+            ));
+          });
+          return averagedForecasts;
         }
+      }else {
+        print('Failed to load weather data: ${response.statusCode}');
 
-        List<WeatherForecast> averagedForecasts = [];
-        groupedForecasts.forEach((date, forecasts) {
-          double avgTemp =
-              forecasts.map((f) => f.temperature).reduce((a, b) => a + b) /
-                  forecasts.length;
-          double avgHumidity =
-              forecasts.map((f) => f.humidity).reduce((a, b) => a + b) /
-                  forecasts.length;
-          double avgWind =
-              forecasts.map((f) => f.wind).reduce((a, b) => a + b) /
-                  forecasts.length;
-          double avgPressure =
-              forecasts.map((f) => f.pressure).reduce((a, b) => a + b) /
-                  forecasts.length;
-          double avgCloudiness =
-              forecasts.map((f) => f.cloudiness).reduce((a, b) => a + b) /
-                  forecasts.length;
-          WeatherCondition avgWeatherCondition =
-          _determineMostFrequentCondition(forecasts);
-
-          averagedForecasts.add(WeatherForecast(
-            date: date,
-            temperature: avgTemp,
-            humidity: avgHumidity,
-            wind: avgWind,
-            pressure: avgPressure,
-            cloudiness: avgCloudiness,
-            weatherCondition: avgWeatherCondition,
-          ));
-        });
-        return averagedForecasts;
       }
+    } catch (e) {
+      print('Error occurred: $e');
     }
     return [];
   }
