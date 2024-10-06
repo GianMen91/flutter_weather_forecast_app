ff15f7c3aaefd2cce14c072840a9bd8ed2ac70d6
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-06 03:46:59 +0200

    remove not necessary classes

--- lib/network/app_url.dart
+++
@@ -1,7 +0,0 @@
-class AppUrl {
-  String get baseUrl => 'htttps://api.openweathermap.org/data/2,5/forecast';
-  String apiKey = '2490e19bf3443658945b392efebeae7c';
-
-  String weatherForecast(double lon, double lat) =>
-      '$baseUrl/?appid=$apiKey&lat=$lat&lon=$lon&exclude=hourly,daily&units=metric';
-}
