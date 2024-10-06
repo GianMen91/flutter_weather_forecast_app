5bdf27155983b3a9785706cdfcb8c6b3bec6aa52
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-06 03:49:00 +0200

    clean main code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_weather_forecast_app/weather_app.dart';
 
-import 'module/module.dart';
-
 void main() {
-  final module = Module();
-  module.initDependencies();
 
-  runApp(WeatherApp(
-    module: module,
+  runApp(const WeatherApp(
   ));
 }
