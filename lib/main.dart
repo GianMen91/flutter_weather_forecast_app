a5a6828818a85b501e7aafc73dc42ddd4eb0ad66
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-07 11:22:35 +0200

    rename class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_weather_forecast_app/weather_app.dart';
+import 'package:flutter_weather_forecast_app/weather_forecast_app.dart';
 
 void main() {
   runApp(const WeatherForecastApp());
