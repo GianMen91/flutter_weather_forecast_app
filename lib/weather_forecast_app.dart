0ba4955843c708e198c6f2f8b5457368c7be6148
GianMen91 <giancarlo.mennillo@gmail.com>
2024-10-07 14:20:33 +0200

    add comments

--- lib/weather_forecast_app.dart
+++ lib/weather_forecast_app.dart
@@ -5,19 +5,23 @@ import 'package:flutter_weather_forecast_app/bloc/weather_bloc.dart';
 import 'package:flutter_weather_forecast_app/repository/weather_repository.dart';
 import 'package:flutter_weather_forecast_app/views/weather_forecast_view.dart';
 
+// Entry point of the Weather Forecast App
 class WeatherForecastApp extends StatelessWidget {
   const WeatherForecastApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    // Create an instance of OpenWeatherApiCall
+    // Create an instance of WeatherRepository which handles API calls.
     final weatherRepository = WeatherRepository();
 
     return BlocProvider(
+      // Initialize the WeatherBloc using the WeatherRepository instance.
       create: (context) => WeatherBloc(openWeatherApiCall: weatherRepository),
-      // Pass the OpenWeatherApiCall instance
+      // Wrap the widget tree with the WeatherBloc provider, so it can be accessed by any widget in the tree.
       child: const MaterialApp(
+        // Disable the debug banner in the app.
         debugShowCheckedModeBanner: false,
+        // Set the home screen of the app to be the WeatherForecastView.
         home: WeatherForecastView(),
       ),
     );
